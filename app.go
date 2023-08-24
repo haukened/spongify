9050abba2dbee5e76c80f066e87b0fd018f1bd53
David Haukeness <david@hauken.us>
2023-08-24 11:06:34 -0600

    initial systray support

--- app.go
+++ app.go
@@ -3,22 +3,40 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
+	"spongify/internal/config"
+
+	"fyne.io/systray"
+	wuntime "github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
 type App struct {
-	ctx context.Context
+	ctx      context.Context
+	conf     *config.Config
+	trayExit func()
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	c, err := config.Load(APP_NAME)
+	if err != nil {
+		panic(err)
+	}
+	return &App{
+		conf: c,
+	}
 }
 
 // startup is called at application startup
 func (a *App) startup(ctx context.Context) {
 	// Perform your setup here
 	a.ctx = ctx
+	// set up the systray
+	start, stop := systray.RunWithExternalLoop(setupSystray(a.ctx), nil)
+	a.trayExit = stop
+	// run the systray
+	start()
 }
 
 // domReady is called after front-end resources have been loaded
@@ -36,9 +54,51 @@ func (a *App) beforeClose(ctx context.Context) (prevent bool) {
 // shutdown is called at application termination
 func (a *App) shutdown(ctx context.Context) {
 	// Perform your teardown here
+	if a.trayExit != nil {
+		a.trayExit()
+	}
 }
 
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func setupSystray(ctx context.Context) func() {
+	return func() {
+		// set the systray icons and settings
+		if runtime.GOOS == "windows" {
+			systray.SetTemplateIcon(iconWin, iconWin)
+		} else {
+			systray.SetTemplateIcon(icon, icon)
+		}
+		systray.SetTitle(APP_DISPLAY_NAME)
+		systray.SetTooltip(APP_DISPLAY_NAME)
+
+		// add menu items
+		mAbout := systray.AddMenuItem("About", "About this app")
+		mAbout.Enable()
+		systray.AddSeparator()
+		mQuit := systray.AddMenuItem("Quit", "Exit the app")
+		mQuit.Enable()
+
+		// background task to listen for click events
+		go func() {
+			for {
+				select {
+				case <-mQuit.ClickedCh:
+					// exit the systray
+					systray.Quit()
+					// exit the app
+					wuntime.Quit(ctx)
+				case <-mAbout.ClickedCh:
+					showAbout(ctx)
+				}
+			}
+		}()
+	}
+}
+
+func showAbout(ctx context.Context) {
+	// here we need to navigate to the about page and then show it
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,25 @@ module spongify
 
 go 1.18
 
-require github.com/wailsapp/wails/v2 v2.5.1
+require (
+	fyne.io/systray v1.10.0
+	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f
+	github.com/knadh/koanf/parsers/toml v0.1.0
+	github.com/knadh/koanf/providers/file v0.1.0
+	github.com/knadh/koanf/v2 v2.0.1
+	github.com/pelletier/go-toml/v2 v2.0.9
+	github.com/wailsapp/wails/v2 v2.5.1
+	golang.design/x/hotkey v0.4.1
+)
 
 require (
 	github.com/bep/debounce v1.2.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
 	github.com/labstack/echo/v4 v4.9.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/leaanthony/go-ansi-parser v1.0.1 // indirect
@@ -16,9 +28,14 @@ require (
 	github.com/leaanthony/slicer v1.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pkg/browser v0.0.0-20210706143420-7d21f8c997e2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/samber/lo v1.27.1 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
 	github.com/tkrajina/go-reflector v0.5.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
@@ -26,7 +43,7 @@ require (
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
 
