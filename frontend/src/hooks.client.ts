1a561b08757a51e19d8aefb65df6ed8437b0b90a
David Haukeness <david@hauken.us>
2023-08-24 12:26:00 -0600

    client navigation hooks

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -1,4 +1,20 @@
-import { WindowReloadApp } from '$lib/wailsjs/runtime/runtime' 
+import { goto } from '$app/navigation'
+import { isValidURL } from '$lib/utils'
+import { EventsOn, WindowReloadApp } from '$lib/wailsjs/runtime/runtime' 
+
+// this force reloads the app on error
 export async function handleError() {
     WindowReloadApp()
+}
+
+// Subscribe the client frontend to navigation events
+EventsOn("navigate", doNavigation)
+
+// handler for navigation event
+function doNavigation(data: any) {
+    if (data && isValidURL(data)) {
+        goto(data)
+    } else {
+        console.log("invalid URI for navigation: ", data);
+    }
 }
