a36043c9f12a11aafd8d24c7b4477742e2727122
David Haukeness <david@hauken.us>
2023-08-25 10:14:32 -0600

    create config page

---
+++ frontend/src/routes/config/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Hotkey } from "$lib/components/hotkeys";
+
+    let keys: string[] = ["ctrl", "shift", "m"]
+    let keysListening: boolean = false;
+
+    const listenToKeys = () => {
+
+    }
+</script>
+
+<div class="flex flex-col items-left space-y-2">
+    <h1 class="text-2xl font-bold underline">Preferences:</h1>
+    <label class="flex flex-row space-x-4 items-center">
+        <input type="checkbox" class="toggle toggle-sm toggle-success"/>
+        <span>Start on login</span>
+    </label>
+    <span>HotKeys</span>
+    <Hotkey/>
+</div>
